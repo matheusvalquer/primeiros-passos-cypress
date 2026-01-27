f1648c69925cce369484b9086f79fb9f78e1c26b
Matheus Valquer <matheus.valquer@gmail.com>
2026-01-27 11:34:32 -0300

    Combo box added

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -14,8 +14,12 @@ describe('Orange HRM Tests', () => {
     lastNameField: "[name='lastName']",
     genericField: ".oxd-input--active",
     dateField: "[placeholder='yyyy-dd-mm']",
-    dateCloseButton: ".--close"
-
+    dateCloseButton: ".--close",
+    genericComboBox: ".oxd-select-text--arrow",
+    brazilianItem: ':nth-child(27) > span',
+    marriedItem: '.oxd-select-dropdown > :nth-child(3)',
+    submitButton: "button.oxd-button"
+    
   }
 
  
@@ -27,18 +31,26 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
-    cy.get(selectorsList.myInfoButton).click()
-    cy.get(selectorsList.firstNameField).clear().type('Name test')
-    cy.get(selectorsList.middleNameField).clear().type('MiddleName test')
-    cy.get(selectorsList.lastNameField).clear().type('LastName test')
+    cy.get(selectorsList.myInfoButton).click({ force: true })
+    cy.get(selectorsList.firstNameField).clear().type('Matheus')
+    cy.get(selectorsList.middleNameField).clear().type('Fierce')
+    cy.get(selectorsList.lastNameField).clear().type('Tester')
     cy.get(selectorsList.genericField).eq(3).clear().type("Employee")
     cy.get(selectorsList.genericField).eq(4).clear().type("OtherId test")
     cy.get(selectorsList.genericField).eq(5).clear().type("Drivers test")
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input').clear().type("2026-22-01")
-    cy.get(selectorsList.dateCloseButton).click()    
-    cy.get(':nth-child(1) > .oxd-form > .oxd-form-actions > .oxd-button').click()
-    cy.get('.oxd-toast')
-
+    cy.get(selectorsList.dateField).eq(0).clear().type("2026-22-01")
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.genericComboBox).eq(0).click()
+    cy.get(selectorsList.brazilianItem).click()
+    cy.get(selectorsList.genericComboBox).eq(1).click()
+    cy.get(selectorsList.marriedItem).click()
+    cy.get(selectorsList.dateField).eq(1).clear().type("2023-07-03")
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Updated')
+    
+  
+    
 
   })
   it('Login - Fail', () => {
